ee9ac2b25bb0f6ec1c173072ee93757bdbd97878
Wang Kang <395684561@qq.com>
2015-04-13 20:04:14 +0800

    Initial Commit

---
+++ My First Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  My First Project
+//
+//  Created by babykang on 15/4/13.
+//  Copyright (c) 2015年 Fiona. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
